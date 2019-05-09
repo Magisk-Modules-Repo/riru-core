4f4451bf2b2bb1acee078c0a15434c921a43e7f0
RikkaW <rikka@shizuku.moe>
2019-05-09 20:32:22 +0800

    v19

---
+++ README.md
@@ -0,0 +1,7 @@
+# Riru - Core
+
+Inject zygote process by replacing libmemtrack, provide the ability for Riru modules to run their code in apps' or system server's process.
+
+All other Riru modules requires Riru - Core installed to work.
+
+See https://github.com/RikkaApps/Riru for more details.
---
+++ common/post-fs-data.sh
@@ -0,0 +1,14 @@
+#!/system/bin/sh
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+# This script will be executed in post-fs-data mode
+#mkdir -p /sbin/riru
+#mount -o bind /sbin/.core/mirror/data/adb/riru /sbin/riru
+
+ZYGOTE_RESTART=/data/misc/riru/bin/zygote_restart
+[[ ! -f "/data/misc/riru/config/disable_auto_restart" ]] && $ZYGOTE_RESTART
